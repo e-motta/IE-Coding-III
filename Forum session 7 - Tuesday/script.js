e2dd7eee84c5e35bcfd270631d9902c8c21a0d45
Eduardo Motta de Moraes <edumoraes@live.com>
2022-02-01 20:28:08 -0300

    add partial forum session 7

---
+++ Forum session 7 - Tuesday/script.js
@@ -0,0 +1,8 @@
+btn1 = document.getElementById("btn1")
+btn1.addEventListener("click", () => document.getElementById("tc").style.borderColor = "red")
+
+btn2 = document.getElementById("btn2")
+btn2.addEventListener("click", () => document.getElementById("tc").style.borderColor = "green")
+
+btn3 = document.getElementById("btn3")
+btn3.addEventListener("click", () => document.getElementById("tc").style.borderColor = "blue")
