29783c1785fa0bae78c34b6ed151ef6922d65221
Eduardo Motta de Moraes <edumoraes@live.com>
2022-02-02 07:39:57 -0300

    finish forum session 7

--- Forum session 7 - Tuesday/script.js
+++ Forum session 7 - Tuesday/script.js
@@ -1,8 +1,21 @@
-btn1 = document.getElementById("btn1")
+let btn1 = document.getElementById("btn1")
 btn1.addEventListener("click", () => document.getElementById("tc").style.borderColor = "red")
 
-btn2 = document.getElementById("btn2")
+let btn2 = document.getElementById("btn2")
 btn2.addEventListener("click", () => document.getElementById("tc").style.borderColor = "green")
 
-btn3 = document.getElementById("btn3")
+let btn3 = document.getElementById("btn3")
 btn3.addEventListener("click", () => document.getElementById("tc").style.borderColor = "blue")
+
+let dropdown = document.getElementById("dropdown")
+
+let maxSize = 11
+
+for (let i = 1; i < maxSize; i++) {
+    let option = document.createElement("option")
+    option.innerText = `${i} px`
+    option.setAttribute("value", `${i}px`)
+    dropdown.appendChild(option)
+}
+
+dropdown.addEventListener("change", () => document.getElementById("tc").style.borderWidth = dropdown.value)
